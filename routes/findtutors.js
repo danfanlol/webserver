420bb6d1879aedaa96c230a5aeb301fbfbb66ad9
enamchae <56808335+enamchae@users.noreply.github.com>
2022-06-19 21:54:54 -0400

    feat: Session viewing page

--- routes/findtutors.js
+++ routes/findtutors.js
@@ -1,11 +1,19 @@
 import express from "express";
 import { baseViewParams } from "../utils/base-view-objects.js";
+import Session from "../model/schema/session.js";
 
 const router = express.Router();
 
-router.get("/", (request, response) => {
+router.get("/", async (request, response) => {
+	const sessions = (await Session.find({}))
+			.sort((a, b) => Number(Boolean(a.student)) - Number(Boolean(b.student)) 
+					|| a.begin - b.begin);
+
 	if (request.isAuthenticated()) {
-		response.render("findtutors/index.ejs", baseViewParams(request));
+		response.render("findtutors/index.ejs", {
+			...baseViewParams(request),
+			sessions,
+		});
 	} else {
 		response.redirect("/login");
 	}
